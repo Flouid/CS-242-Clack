742c8b80031dac6f6777e14d8e0985fd707e7144
Louis Keith <keithla@clarkson.edu>
2020-08-25 14:09:32 -0400

    Working on the ClackData Class

--- src/data/ClackData.java
+++ src/data/ClackData.java
@@ -1,7 +1,28 @@
 package data;
+
+import java.util.Date;
+
 /**
  * @author Louis Keith
  */
 
 public class ClackData {
+
+    // constant declarations
+    static int CONSTANT_LISTUSERS = 1;
+    static int CONSTANT_LOGOUT = 2;
+    static int CONSTANT_SENDMESSAGE = 3;
+    static int CONSTANT_SENDFILE = 4;
+
+    // instance variable declarations
+    String userName;
+    int type;
+    Date date;
+
+    // constructor to set up userName and type, date is automatically created
+    ClackData(String userName, int type) {
+        this.userName = userName;
+        this.type = type;
+        this.date = new Date();
+    }
 }
