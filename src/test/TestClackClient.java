c8bf0d2cb45091dd693341732b435cfed2854f20
Louis Keith <keithla@clarkson.edu>
2020-09-08 13:06:11 -0400

    Added TestClackData and TestClackClient

---
+++ src/test/TestClackClient.java
@@ -0,0 +1,15 @@
+package test;
+
+import main.ClackClient;
+
+/**
+ * This class tests the TestClackClient class
+ *
+ * @author Louis Keith
+ */
+public class TestClackClient {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/TestClackData.java
@@ -0,0 +1,16 @@
+package test;
+
+import data.*;
+
+/**
+ * This class tests the MessageClackData and FileClackData classes.
+ *
+ * @author Louis Keith
+ */
+public class TestClackData {
+
+    public static void main(String[] args) {
+
+
+    }
+}
