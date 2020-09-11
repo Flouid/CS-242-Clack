81cc6cca27581d6e7824c71a96196dd2e0880beb
Louis Keith <keithla@clarkson.edu>
2020-09-11 09:48:13 -0400

    Fixed TestClackData

--- src/test/TestClackData.java
+++ src/test/TestClackData.java
@@ -78,8 +78,6 @@ public class TestClackData {
         System.out.println("Type of testFileClackData1: " + testFileClackData1.getType());
         System.out.println("User name of testFileClackData1: " + testFileClackData1.getUserName());
         System.out.println("Date of testFileClackData1: " + testFileClackData1.getDate());
-        // getFileName() is specific to the subclass, it cant be accessed :\
-        //System.out.println("File Name of testFileClackData1: " + testFileClackData1.getFileName());
         System.out.println("File contents of testMessageClackData1: " + testFileClackData1.getData());
         System.out.println("Hash code of testMessageClackData1: " + testFileClackData1.hashCode());
         System.out.println("testMessageClackData1's toString method: \n" + testFileClackData1.toString());
@@ -89,8 +87,6 @@ public class TestClackData {
         System.out.println("Type of testFileClackData2: " + testFileClackData2.getType());
         System.out.println("User name of testFileClackData2: " + testFileClackData2.getUserName());
         System.out.println("Date of testFileClackData2: " + testFileClackData2.getDate());
-        // getFileName() is specific to the subclass, it cant be accessed :\
-        //System.out.println("File Name of testFileClackData2: " + testFileClackData2.getFileName());
         System.out.println("File contents of testMessageClackData2: " + testFileClackData2.getData());
         System.out.println("Hash code of testMessageClackData2: " + testFileClackData2.hashCode());
         System.out.println("testMessageClackData2's toString method: \n" + testFileClackData2.toString());
@@ -100,28 +96,12 @@ public class TestClackData {
                 + testFileClackData1.equals(testFileClackData2));
         System.out.print("\n");
 
-        // Test FileClackData's setFileName method
-        System.out.println("Changing testFileClackData1's file name to: " + testFileName2);
-        // Same issue as getFileName()
-        //testFileClackData1.setFileName(testFileName2);
-        System.out.println("Type of testFileClackData1: " + testFileClackData1.getType());
-        System.out.println("User name of testFileClackData1: " + testFileClackData1.getUserName());
-        System.out.println("Date of testFileClackData1: " + testFileClackData1.getDate());
-        // getFileName() is specific to the subclass, it cant be accessed :\
-        //System.out.println("File Name of testFileClackData1: " + testFileClackData1.getFileName());
-        System.out.println("File contents of testMessageClackData1: " + testFileClackData1.getData());
-        System.out.println("Hash code of testMessageClackData1: " + testFileClackData1.hashCode());
-        System.out.println("testMessageClackData1's toString method: \n" + testFileClackData1.toString());
-        System.out.print("\n\n");
-
 
         // Test FileClackData's default constructor
         ClackData testFileClackData3 = new FileClackData();
         System.out.println("Type of testFileClackData3: " + testFileClackData3.getType());
         System.out.println("User name of testFileClackData3: " + testFileClackData3.getUserName());
         System.out.println("Date of testFileClackData3: " + testFileClackData3.getDate());
-        // getFileName() is specific to the subclass, it cant be accessed :\
-        //System.out.println("File Name of testFileClackData3: " + testFileClackData3.getFileName());
         System.out.println("File contents of testMessageClackData3: " + testFileClackData3.getData());
         System.out.println("Hash code of testMessageClackData3: " + testFileClackData3.hashCode());
         System.out.println("testMessageClackData3's toString method: \n" + testFileClackData3.toString());
@@ -131,8 +111,6 @@ public class TestClackData {
         System.out.println("Type of testFileClackData4: " + testFileClackData4.getType());
         System.out.println("User name of testFileClackData4: " + testFileClackData4.getUserName());
         System.out.println("Date of testFileClackData4: " + testFileClackData4.getDate());
-        // getFileName() is specific to the subclass, it cant be accessed :\
-        //System.out.println("File Name of testFileClackData4: " + testFileClackData4.getFileName());
         System.out.println("File contents of testMessageClackData4: " + testFileClackData4.getData());
         System.out.println("Hash code of testMessageClackData4: " + testFileClackData4.hashCode());
         System.out.println("testMessageClackData4's toString method: \n" + testFileClackData4.toString());
@@ -141,19 +119,5 @@ public class TestClackData {
         System.out.println("testFileClackData3 and testFileClackData4 are equal: "
                 + testFileClackData3.equals(testFileClackData4));
         System.out.print("\n");
-
-        // Test FileClackData's setFileName method
-        System.out.println("Changing testFileClackData3's file name to: " + testFileName2);
-        // Same issue as getFileName()
-        //testFileClackData3.setFileName(testFileName2);
-        System.out.println("Type of testFileClackData3: " + testFileClackData3.getType());
-        System.out.println("User name of testFileClackData3: " + testFileClackData3.getUserName());
-        System.out.println("Date of testFileClackData3: " + testFileClackData3.getDate());
-        // getFileName() is specific to the subclass, it cant be accessed :\
-        //System.out.println("File Name of testFileClackData3: " + testFileClackData3.getFileName());
-        System.out.println("File contents of testMessageClackData3: " + testFileClackData3.getData());
-        System.out.println("Hash code of testMessageClackData3: " + testFileClackData3.hashCode());
-        System.out.println("testMessageClackData3's toString method: \n" + testFileClackData3.toString());
-        System.out.print("\n\n");
     }
 }
