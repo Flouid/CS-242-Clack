310a11f84c90411bdc9e38a4f8ae037bbbddcacd
Alex Cohen <alexander.c.3600@gmail.com>
2020-11-03 17:34:47 -0500

    started to create the basic structure of the GUI

--- src/main/ClackGUI.java
+++ src/main/ClackGUI.java
@@ -29,10 +29,10 @@ public class ClackGUI extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         ArrayList<String> participants = new ArrayList<String>();
-        participants.add("Alex");
-        participants.add("Louis");
-        participants.add("Dohan");
-        participants.add("Natasha");
+//        participants.add("Alex");
+//        participants.add("Louis");
+//        participants.add("Dohan");
+//        participants.add("Natasha");
         window = primaryStage;
         GridPane layout = new GridPane();
         TextField textField = new TextField();
@@ -40,22 +40,35 @@ public class ClackGUI extends Application {
         ObservableList<String> observableList = FXCollections.observableList(participants);
         ListView<String> listView = new ListView<String>();
         listView.setItems(observableList);
-        listView.setMinWidth(100);
+        listView.setMaxWidth(100);
         listView.setPrefHeight(140);
+
+        TextArea messages = new TextArea();
+        messages.setMaxWidth(150);
+        messages.setPrefHeight(140);
 
         layout.setPadding(new Insets(10, 10, 10, 10));
         layout.setVgap(10);
         layout.setHgap(10);
-        layout.getChildren().addAll(textField, send, listView);
+        layout.getChildren().addAll(textField, send, listView, messages);
         GridPane.setConstraints(listView, 0, 0);
+        GridPane.setConstraints(messages, 1, 0);
         GridPane.setConstraints(send, 0, 1);
         GridPane.setConstraints(textField, 1, 1);
+
+        send.setOnAction(e -> {
+            System.out.println(textField.getText());
+        });
 
 
         scene = new Scene(layout);
         window.setTitle("Clack");
         window.setScene(scene);
         window.show();
+    }
+
+    public static void send(GridPane layout) {
+
     }
 }
 
